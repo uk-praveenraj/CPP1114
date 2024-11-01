0f49957450f12d5a864d75495f2a861e9046d330
Praveen Pillai <uk.praveenraj@gmail.com>
2024-11-01 09:19:05 +0000

    updated for video

--- cpp_world/pointers/2dpointer.cpp
+++ cpp_world/pointers/2dpointer.cpp
@@ -4,6 +4,16 @@ using namespace std;
 int main(){
     int rows = 3, cols=3;
 
+    // int matrix[rows][cols];
+    // int k=0;
+    // for(int i=0; i<rows;++i){
+    //     for(int j=0; j<cols;++j){
+    //         matrix[i][j] = k++;
+    //         cout << matrix[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
     int **table = new int*[rows];
     for(int i=0; i<rows;++i){
         table[i] = new int[cols];
